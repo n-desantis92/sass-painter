3643c391776da985c5d691346fb5850730a30902
n-desantis92 <dsnwebdev@gmail.com>
2021-04-17 12:41:15 +0200

    aggiunto oggetto sezioni

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 var app = new Vue({
   el: '#app',
   data: {
-
+    sezioni: [
+      {
+        titleSez: "Ritocca, sostituisci, assembla, wow!",
+        parag: "Realizza ritratti da manuale con soggetti più espressivi, sostituisci il cielo con un clic, rimuovi gli oggetti, applica ritocchi e ricolora una scena. Grazie a strumenti di fotoritocco avanzati e a nuove funzionalità intelligenti per la creazione di effetti magici, puoi trasformare le immagini in opere d’arte.",
+        video: "https://images-tv.adobe.com/mpcv3/1041/306aed77-cd2b-4298-9097-48ae9680c011_1600721058.960x540at1200_h264.mp4",
+        img: "",
+      },
+      {
+        titleSez: "Fai correre la tua immaginazione per trasformare le tue foto.",
+        parag: "Sfruttando un numero virtualmente illimitato di livelli, selezioni con un clic e facili operazioni di mascheratura, puoi creare rapidamente quello che la tua fotocamera non è in grado di immortalare.",
+        video: "https://images-tv.adobe.com/mpcv3/1041/306aed77-cd2b-4298-9097-48ae9680c011_1600721058.960x540at1200_h264.mp4",
+        img: "pavone.webp",
+      },
+      {
+        titleSez: "Mille tocchi di bravura.",
+        parag: "Dipingi e disegna con migliaia di pennelli personalizzati o creane di nuovi: con Photoshop hai a disposizione la più grande raccolta al mondo.",
+        video: "https://images-tv.adobe.com/mpcv3/1041/306aed77-cd2b-4298-9097-48ae9680c011_1600721058.960x540at1200_h264.mp4",
+        img: "",
+      },
+      {
+        titleSez: "Il tuo punto di riferimento per progettare grafiche di grande impatto.",
+        parag: "Poster, packaging, banner, siti web: la progettazione grafica inizia con Photoshop. Combina foto, grafiche e testi per creare immagini completamente nuove, seleziona i soggetti con un clic e gioca con colori ed effetti per perfezionare ogni progetto di design.",
+        video: "https://images-tv.adobe.com/mpcv3/1041/306aed77-cd2b-4298-9097-48ae9680c011_1600721058.960x540at1200_h264.mp4",
+        img: "spotify.webp",
+      },
+    ],
   },
   methods: {
-    
+
   }
 })
